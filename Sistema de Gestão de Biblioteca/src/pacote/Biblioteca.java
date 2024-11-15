b071aaadb29a22727d847cdc30dc0d45388cb908
sauls <sauls@Jarbas>
2024-11-15 16:17:01 -0300

    Correção da BibliotecaApp e adição do método pesquisar livros

---
+++ Sistema de Gestão de Biblioteca/src/pacote/Biblioteca.java
@@ -0,0 +1,137 @@
+package pacote;
+
+import java.time.LocalDate;
+import java.time.temporal.ChronoUnit;
+import java.util.ArrayList;
+
+class Biblioteca {
+    private ArrayList<Livro> livros;
+    private ArrayList<Cliente> clientes;
+
+    public Biblioteca() {
+        livros = new ArrayList<>();
+        clientes = new ArrayList<>();
+    }
+
+    public void cadastrarLivro(Livro livro) {
+        livros.add(livro);
+    }
+
+    public void listarLivros() {
+        for (Livro livro : livros) {
+            System.out.println(livro);
+        }
+    }
+
+    public void cadastrarCliente(Cliente cliente) {
+        clientes.add(cliente);
+    }
+
+    public void listarClientes() {
+        for (Cliente cliente : clientes) {
+            System.out.println(cliente);
+        }
+    }
+
+    public Cliente buscarCliente(String cpf) {
+        return clientes.stream().filter(c -> c.getCpf().equals(cpf)).findFirst().orElse(null);
+    }
+
+    public Livro buscarLivro(String titulo) {
+        return livros.stream().filter(l -> l.getTitulo().equalsIgnoreCase(titulo)).findFirst().orElse(null);
+    }
+
+    public void emprestarLivro(String cpf, String titulo) {
+        Cliente cliente = buscarCliente(cpf);
+        Livro livro = buscarLivro(titulo);
+
+        if (cliente == null || livro == null) {
+            System.out.println("Cliente ou livro não encontrado.");
+            return;
+        }else if (!cliente.podeEmprestar()) {
+            System.out.println("Cliente não pode pegar mais livros emprestados.");
+            return;
+        }else if (livro.getExemplaresDisponiveis() == 0) {
+            System.out.println("Livro indisponível.");
+            return;
+        }else if (cliente.getMultaPendente() > 0.0) {
+        	System.out.println("Cliente com pendências, não pode pegar mais livros emprestados");
+        }else {
+        	cliente.emprestarLivro(livro);
+        	livro.emprestar();
+        	System.out.println("Livro emprestado com sucesso!");
+        }
+    }
+
+    public void devolverLivro(String cpf, String titulo) {
+        Cliente cliente = buscarCliente(cpf);
+        Livro livro = buscarLivro(titulo);
+
+        if (cliente == null || livro == null) {
+            System.out.println("Cliente ou livro não encontrado.");
+            return;
+        }else if (!cliente.getLivrosEmprestados().contains(livro)) {
+            System.out.println("Cliente não possui este livro.");
+            return;
+        }else {
+        	cliente.devolverLivro(livro);
+        	livro.devolver();
+        	System.out.println("Livro devolvido com sucesso!");
+        }
+    }
+    
+    public void consultarMulta(String cpf) {
+         Cliente cliente = buscarCliente(cpf);
+         if (cliente != null) {
+             System.out.printf("Saldo de Multas: R$ %.2f%n", cliente.getMultaPendente());
+         } else {
+             System.out.println("Cliente não encontrado.");
+         }
+    }
+    
+    public void pagarMulta(String cpf) {
+    	Cliente cliente = buscarCliente(cpf);
+        if (cliente != null) {
+            cliente.quitarMulta();
+            System.out.println("Multa quitada com sucesso!");
+        } else {
+            System.out.println("Cliente não encontrado.");
+        }
+    }
+    
+    public void calcularMulta(String cpf, LocalDate dataDevolucao, LocalDate dataAtual) {
+    	Cliente cliente = buscarCliente(cpf);
+    	if (cliente != null) {
+    		long diasAtraso = ChronoUnit.DAYS.between(dataDevolucao, dataAtual);
+    		double valorDiario = cliente.getMultaDiaria();
+        	double limiteDias = cliente.getPrazoDias();
+    		if (diasAtraso > limiteDias) {
+    			double valorMulta = valorDiario * (diasAtraso - limiteDias);
+            	cliente.adicionarMulta(valorMulta);
+            	System.out.printf("Multa aplicada: R$ %.2f%n", valorMulta);
+            }
+        } else {
+        	System.out.println("Cliente não encontrado");
+        }
+    }
+    
+    public void pesquisarLivro(String criterio, String termo) {
+        boolean encontrado = false;
+
+        for (Livro livro : livros) {
+            if (criterio.equals("título") && livro.getTitulo().toLowerCase().contains(termo)) {
+                System.out.println(livro);
+                encontrado = true;
+            } else if (criterio.equals("autor") && livro.getAutor().toLowerCase().contains(termo)) {
+                System.out.println(livro);
+                encontrado = true;
+            }
+        }
+
+        if (!encontrado) {
+            System.out.println("Nenhum livro encontrado com o critério fornecido.");
+        }
+    }
+
+
+}
--- Sistema de Gestão de Biblioteca/src/pacote/BibliotecaApp.java
+++ Sistema de Gestão de Biblioteca/src/pacote/BibliotecaApp.java
@@ -25,123 +25,133 @@ public class BibliotecaApp {
             System.out.println("9. Pesquisar Livro por Título ou Autor");
             System.out.println("10. Sair");
             System.out.print("Escolha uma opção: ");
-        }
+            
+            int opcao = scanner.nextInt();
+            scanner.nextLine();    
+            
+            switch (opcao) {
+	        	case 1:
+	        		System.out.println("Título: ");
+	        		String titulo = scanner.nextLine();
+	            
+	        		System.out.print("Autor: ");
+	        		String autor = scanner.nextLine();
+	            
+	        		System.out.print("Gênero (Romance, Ficcao, Biografia, Tecnico, Terror): ");
+	        		String generoInput = scanner.nextLine().toUpperCase();
+	        		try {
+		            	Livro.Genero genero = Livro.Genero.valueOf(generoInput);
+		            } catch (IllegalArgumentException e) {
+		                System.out.println("Tipo de gênero inválido. Por favor, digite Romance, Ficcao, Biografia, Tecnico ou Terror.");
+		            }
+	        		Livro.Genero genero = Livro.Genero.valueOf(generoInput);
+	            
+	        		System.out.print("Ano de Publicação: ");
+	        		int ano = scanner.nextInt();
+	            
+	        		System.out.print("Número de Exemplares: ");
+	        		int exemplares = scanner.nextInt();
 	
-        int opcao = scanner.nextInt();
-        scanner.nextLine();    
+	        		Livro livro = new Livro(titulo, autor, genero, ano, exemplares);
+	        		biblioteca.cadastrarLivro(livro);
+	        		System.out.println("Livro cadastrado com sucesso!");
+	        		break;
+            
+		        case 2:
+		        	System.out.print("Nome: ");
+		            String nome = scanner.nextLine();
+		            
+		            System.out.print("CPF: ");
+		            String cpf = scanner.nextLine();
+		            
+		            System.out.print("Tipo de Cliente (ALUNO, PROFESSOR): ");
+		            String tipoClienteInput = scanner.nextLine().toUpperCase();
+		            try {
+		            	Cliente.TipoCliente tipoCliente = Cliente.TipoCliente.valueOf(tipoClienteInput);
+		            } catch (IllegalArgumentException e) {
+		                System.out.println("Tipo de cliente inválido. Por favor, digite ALUNO ou PROFESSOR.");
+		            }
+		            Cliente.TipoCliente tipoCliente = Cliente.TipoCliente.valueOf(tipoClienteInput);
+		            
+		            Cliente cliente;
+		            if (tipoClienteInput == "ALUNO") {
+		                cliente = new Aluno(nome, cpf, tipoCliente);
+		            } else {
+		                cliente = new Professor(nome, cpf, tipoCliente);
+		            }
+		
+		            biblioteca.cadastrarCliente(cliente);
+		            System.out.println("Cliente cadastrado com sucesso!");
+		            break;
+		            
+		        case 3:
+		        	System.out.print("CPF do Cliente: ");
+		            String CPF = scanner.nextLine();
+		            
+		            System.out.print("Título do Livro: ");
+		            String tituloE = scanner.nextLine();
+		
+		            biblioteca.emprestarLivro(CPF, tituloE);
+		            break;
+		            
+		        case 4:
+		        	System.out.print("CPF do Cliente: ");
+		            String CPFd = scanner.nextLine();
+		            
+		            System.out.print("Título do Livro: ");
+		            String tituloD = scanner.nextLine();
+		            
+		            LocalDate dataAtual = LocalDate.now();
+		            System.out.println("Dia da Devolução (aaaa-mm-dd): ");
+		            String dataDevolucaoInput = scanner.nextLine();
+		            LocalDate dataDevolucao = LocalDate.parse(dataDevolucaoInput);
+		
+		            biblioteca.devolverLivro(CPFd, tituloD);
+		            biblioteca.calcularMulta(CPFd, dataDevolucao, dataAtual);
+		            break;
+		            
+		        case 5:
+		        	System.out.print("CPF do Cliente: ");
+		            String CPFc = scanner.nextLine();
+		
+		            biblioteca.consultarMulta(CPFc);
+		            break;
+		            
+		        case 6:
+		        	System.out.print("CPF do Cliente: ");
+		            String CPFq = scanner.nextLine();
+		
+		            biblioteca.pagarMulta(CPFq);
+		            break;
+		            
+		        case 7:
+		        	biblioteca.listarLivros();
+		        	break;
+		        	
+		        case 8:
+		        	biblioteca.listarClientes();
+		        	break;
+		            
+		        case 9:
+		        	System.out.print("Deseja pesquisar por título ou autor? (Digite 'titulo' ou 'autor'): ");
+		        	String criterio = scanner.nextLine().toLowerCase();
+		        	
+		        	System.out.print("Digite o título ou autor do livro: ");
+		            String termo = scanner.nextLine();
+		
+		            biblioteca.pesquisarLivro(criterio, termo);
+		            break;
+		            
+		        case 10:
+		        	System.out.println("Saindo do sistema...");
+		        	scanner.close();
+		        	System.exit(0);
+		        	
+		        default:
+		        	System.out.println("Opção inválida. Tente novamente.");
+		        	break;
+        }
 	}
 
-	switch (opcao) {
-    case 1:
-    	System.out.print("Título: ");
-        String titulo = scanner.nextLine();
-        System.out.print("Autor: ");
-        String autor = scanner.nextLine();
-        System.out.print("Gênero (Romance, Ficcao, Biografia, Tecnico): ");
-        Genero genero = Genero.valueOf(scanner.nextLine().toUpperCase());
-        System.out.print("Ano de Publicação: ");
-        int ano = scanner.nextInt();
-        System.out.print("Número de Exemplares: ");
-        int exemplares = scanner.nextInt();
-
-        Livro livro = new Livro(titulo, autor, genero, ano, exemplares);
-        biblioteca.adicionarLivro(livro);
-        System.out.println("Livro cadastrado com sucesso!");
-        break;
-        
-    case 2:
-    	System.out.print("Nome: ");
-        String nome = scanner.nextLine();
-        System.out.print("CPF: ");
-        String cpf = scanner.nextLine();
-        System.out.print("Tipo de Cliente (ALUNO, PROFESSOR): ");
-        TipoCliente tipo = TipoCliente.valueOf(scanner.nextLine().toUpperCase());
-
-        Cliente cliente;
-        if (tipo == TipoCliente.ALUNO) {
-            cliente = new Aluno(nome, cpf);
-        } else {
-            cliente = new Professor(nome, cpf);
-        }
-
-        biblioteca.cadastrarCliente(cliente);
-        System.out.println("Cliente cadastrado com sucesso!");
-        break;
-        
-    case 3:
-    	System.out.print("CPF do Cliente: ");
-        String cpf = scanner.nextLine();
-        System.out.print("Título do Livro: ");
-        String titulo = scanner.nextLine();
-
-        try {
-            biblioteca.realizarEmprestimo(cpf, titulo);
-            System.out.println("Empréstimo realizado com sucesso!");
-        } catch (RuntimeException e) {
-            System.out.println("Erro: " + e.getMessage());
-        }
-        break;
-        
-    case 4:
-    	System.out.print("CPF do Cliente: ");
-        String cpf = scanner.nextLine();
-        System.out.print("Título do Livro: ");
-        String titulo = scanner.nextLine();
-
-        try {
-            biblioteca.realizarDevolucao(cpf, titulo, LocalDate.now());
-            System.out.println("Devolução realizada com sucesso!");
-        } catch (RuntimeException e) {
-            System.out.println("Erro: " + e.getMessage());
-        }
-        break;
-        
-    case 5:
-    	System.out.print("CPF do Cliente: ");
-        String cpf = scanner.nextLine();
-
-        Cliente cliente = biblioteca.buscarCliente(cpf);
-        if (cliente != null) {
-            System.out.printf("Saldo de Multas: R$ %.2f%n", cliente.getMultaPendente());
-        } else {
-            System.out.println("Cliente não encontrado.");
-        }
-        break;
-        
-    case 6:
-    	System.out.print("CPF do Cliente: ");
-        String cpf = scanner.nextLine();
-
-        Cliente cliente = biblioteca.buscarCliente(cpf);
-        if (cliente != null) {
-            cliente.pagarMulta();
-            System.out.println("Multa quitada com sucesso!");
-        } else {
-            System.out.println("Cliente não encontrado.");
-        }
-        break;
-        
-    case 7:
-    	blioteca.ListarLivros();
-    	break;
-    	
-    case 8:
-    	biblioteca.ListarClientes();
-    	break;
-        
-    case 9:
-    	System.out.print("Digite o título ou autor do livro: ");
-        String termo = scanner.nextLine();
-
-        biblioteca.pesquisarLivros(termo).forEach(System.out::println);
-        break;
-        
-    case 10:
-    	System.out.println("Saindo do sistema...");
-    	break;
-    	
-    default:
-    	System.out.println("Opção inválida. Tente novamente.");
-    	break;
     }      
 }
