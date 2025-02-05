47eae21271816b328e89e1904eb20914be7d5de8
LABORATORIO <LABORATORIO@DESKTOP-T3M2G8V>
2025-02-05 11:40:40 -0300

    Adição do botão "buscar" em "Devolução de Equipamentos"

--- TrabalhoPraticoII/src/main/java/gui/SistemaConstrutecGUI.java
+++ TrabalhoPraticoII/src/main/java/gui/SistemaConstrutecGUI.java
@@ -736,7 +736,7 @@ public class SistemaConstrutecGUI extends javax.swing.JFrame {
     }//GEN-LAST:event_btnConfirmarActionPerformed
 
     private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
-        // TODO add your handling code here:
+        
     }//GEN-LAST:event_jButton2ActionPerformed
 
     
