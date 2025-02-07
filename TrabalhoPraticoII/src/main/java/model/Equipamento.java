ab8fd1c9d5bd0e965f1d5ceb3bbb2b092022d209
Saul-Duarte <147617447+Saul-Duarte@users.noreply.github.com>
2025-02-07 15:53:56 -0300

    Add files via upload
    Adição do campo quantidade nos métodos de equipamento.

--- TrabalhoPraticoII/src/main/java/model/Equipamento.java
+++ TrabalhoPraticoII/src/main/java/model/Equipamento.java
@@ -5,29 +5,31 @@ import java.sql.*;
 import javax.swing.table.DefaultTableModel;
 
 public class Equipamento {
-   public void inserirEquipamento(String nome, String descricao, double valor_diario, boolean status) {
-       String sql = "INSERT INTO equipamento (nome, descricao, valor_diario) VALUES (?, ?, ?)";
+   public void inserirEquipamento(String nome, String descricao, double valor_diario, boolean status, int quantidade) {
+       String sql = "INSERT INTO equipamento (nome, descricao, valor_diario, status, quantidade) VALUES (?, ?, ?, ?, ?)";
        try (Connection conn = ConexaoMySQL.conectar();
         PreparedStatement stmt = conn.prepareStatement(sql)) {
             stmt.setString(1, nome);
             stmt.setString(2, descricao);
             stmt.setDouble(3, valor_diario);
             stmt.setBoolean(4, status);
+            stmt.setInt(5, quantidade);
             stmt.executeUpdate();
        } catch (Exception e) {
            e.printStackTrace();
        }
    }
    
-   public void atualizarEquipamento(String nome, int id, String descricao, double valor_diario, boolean status) {
-        String sql = "UPDATE usuarios SET nome=?, descricao=?, valor_diario=?, status=? WHERE id=?";
+   public void atualizarEquipamento(String nome, int id, String descricao, double valor_diario, boolean status, int quantidade) {
+        String sql = "UPDATE usuarios SET nome=?, descricao=?, valor_diario=?, status=?, quantidade=? WHERE id=?";
         try (Connection conn = ConexaoMySQL.conectar();
              PreparedStatement stmt = conn.prepareStatement(sql)) {
             stmt.setString(1, nome);
             stmt.setString(2, descricao);
             stmt.setDouble(3, valor_diario);
             stmt.setBoolean(4, status);
-            stmt.setInt(5, id);
+            stmt.setInt(5, quantidade);
+            stmt.setInt(6, id);
             stmt.executeUpdate();
         } catch (Exception e) {
             e.printStackTrace();
@@ -52,7 +54,7 @@ public class Equipamento {
              PreparedStatement stmt = conn.prepareStatement(sql);
              ResultSet rs = stmt.executeQuery()) {
             while (rs.next()) {
-                model.addRow(new Object[]{rs.getString("nome"), rs.getInt("id"), rs.getString("descricao"), rs.getDouble("valor_diario"), rs.getBoolean("status")});
+                model.addRow(new Object[]{rs.getString("nome"), rs.getInt("id"), rs.getString("descricao"), rs.getDouble("valor_diario"), rs.getBoolean("status"), rs.getInt("quantidade")});
             }
         } catch (Exception e) {
             e.printStackTrace();
